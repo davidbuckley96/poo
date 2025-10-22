d35cc51743fb6112603c30e1e2858042ae4932f3
David Buckley <david.buckley@goodbarber.com>
2025-10-22 23:01:04 +0100

    add planta header file

---
+++ planta.h
@@ -0,0 +1,13 @@
+//
+// Created by GoodBarber on 22/10/2025.
+//
+
+#ifndef TRABALHO_PLANTA_H
+#define TRABALHO_PLANTA_H
+
+class Planta {
+public:
+    virtual ~Planta() = default;
+};
+
+#endif //TRABALHO_PLANTA_H
